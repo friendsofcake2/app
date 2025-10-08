20290a63669fe63a694a83a0f86f7c055cbddac8
TSURU <tsuyoshi.tsurushima@gmail.com>
2025-10-09 06:20:55 +0900

    Update test configuration paths for modern CakePHP structure
    - Fix database.php path in CI workflow to use tests/config instead of lib/Cake/Test/Config
    - Use Composer\InstalledVersions to dynamically locate CakePHP test bootstrap instead of hardcoded vendor path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,10 @@
  * Bootstrap for PHPUnit
  */
 
+use Composer\InstalledVersions;
+
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__DIR__));
 define('VENDORS', ROOT . DS . 'vendor' . DS);
 
-require_once 'vendor' . DS . 'pieceofcake2' . DS . 'cakephp' . DS . 'tests' . DS . 'bootstrap.php';
+require_once InstalledVersions::getInstallPath('pieceofcake2/cakephp') . DS . 'tests' . DS . 'bootstrap.php';
